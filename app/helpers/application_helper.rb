450edbd4a8899d656604cc41d3a9625e2a2cdfe9
Eric Mulligan <eric.pierre.mulligan@gmail.com>
2016-02-19 07:26:26 -0500

    Added RedCarpet for Markdown rendering and posts are now written using markdown.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def mark_it_down(markeddown)
+    renderer = Redcarpet::Render::HTML.new(filter_html: true)
+    markdown = Redcarpet::Markdown.new(renderer)
+    markdown.render(markeddown)
+  end
 end
