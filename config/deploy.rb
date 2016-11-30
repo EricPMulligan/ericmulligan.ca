0cbc564a7670f637645df9d0718577e3b94d1c1d
Eric Mulligan <eric.pierre.mulligan@gmail.com>
2016-11-30 07:56:25 -0500

    Removed mention set :branch and uncommented the line above.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,7 @@ set :application, 'ericmulligan'
 set :repo_url, 'git@github.com:EricPMulligan/ericmulligan.ca.git'
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, 'release'
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/ericmulligan/www'
